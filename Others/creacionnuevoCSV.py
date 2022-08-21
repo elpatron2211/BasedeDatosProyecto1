5342e73fa32f60eac2c705aa5b5cea8355aaf88e
Raul <san21401@uvg.edu.gt>
2022-08-21 09:48:51 -0600

    Creacion de tablas Teams
    Generar tabla teams

--- Others/creacionnuevoCSV.py
+++ Others/creacionnuevoCSV.py
@@ -1,8 +1,16 @@
 import pandas as pd
+#Creacion de tabla Salary
 PlayerAttributes=pd.read_csv("C:\\Users\\raule\\Desktop\\CuartoSemestre2022\\BasedeDatos\\Proyecto1\\Player_Attributes.csv")
 PlayerSalary=pd.read_csv("C:\\Users\\raule\\Desktop\\CuartoSemestre2022\\BasedeDatos\Proyecto1\\Player_Salary.csv")
 PlayerSalary= PlayerSalary.join(PlayerAttributes.set_index('DISPLAY_FIRST_LAST'), on='namePlayer')
-print(PlayerSalary)
+#print(PlayerSalary)
 Salary=pd.DataFrame().assign(Season=PlayerSalary['slugSeason'],PlayerID=PlayerSalary['ID'],Contract_Type=PlayerSalary['typeContractDetail'],Value=PlayerSalary['value'])
 Salary.to_csv('Salary.csv')
-print(Salary)
+#print(Salary)
+#Creacion de Tabla Team
+TeamAttributes=pd.read_csv("C:\\Users\\raule\\Desktop\\CuartoSemestre2022\\BasedeDatos\\Proyecto1\\Team_Attributes.csv")
+Team=pd.read_csv("C:\\Users\\raule\\Desktop\\CuartoSemestre2022\\BasedeDatos\\Proyecto1\\Team.csv")
+TeamAttributes=TeamAttributes.join(Team.set_index('id'),on='ID')
+Teams=pd.DataFrame().assign(Team_ID=TeamAttributes['ID'],Full_Name=TeamAttributes['full_name'],Abbreviation=TeamAttributes['abbreviation'],Nickname=TeamAttributes['NICKNAME'],City=TeamAttributes['CITY'],State=TeamAttributes['state'],Arena=TeamAttributes['state'])
+Teams.to_csv("Teams.csv")
+print(Teams)
